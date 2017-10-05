ca916435a004088add9ef38c845d0e9434391818
Luca Weiss <luca@z3ntu.xyz>
2017-10-05 15:39:03 +0200

    Initial commit
    Includes an auto-generated project structre, made by IntelliJ and a
    simple README.md file
    
    Signed-off-by: Luca Weiss <luca@z3ntu.xyz>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hand-ins
+
+This repository contains hand-ins for Luca Weiss for **POS1** in the year **2017/18**.
---
+++ src/main/java/at/spengergasse/weiss/PosHandInsApplication.java
@@ -0,0 +1,12 @@
+package at.spengergasse.weiss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosHandInsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosHandInsApplication.class, args);
+	}
+}
