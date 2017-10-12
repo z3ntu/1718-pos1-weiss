e5753f4056d7e24799f334255cf9b4635e730cb6
Luca Weiss <luca@z3ntu.xyz>
2017-10-12 16:00:23 +0200

    Add Repository classes
    Defines repositories which use the classes from the domain layer.
    
    Signed-off-by: Luca Weiss <luca@z3ntu.xyz>

---
+++ src/main/java/at/spengergasse/weiss/persistence/MessageRepository.java
@@ -0,0 +1,11 @@
+package at.spengergasse.weiss.persistence;
+
+import at.spengergasse.weiss.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long>, QueryDslPredicateExecutor<Message> {
+
+}
