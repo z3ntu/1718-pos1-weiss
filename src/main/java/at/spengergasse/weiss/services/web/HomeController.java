18d69a4870e6f847f7d5074fe1038c5f7f2497da
Luca Weiss <luca@z3ntu.xyz>
2018-02-14 20:28:42 +0100

    Replace @Controller and @ResponseBody with @RestController

--- src/main/java/at/spengergasse/weiss/services/web/HomeController.java
+++ src/main/java/at/spengergasse/weiss/services/web/HomeController.java
@@ -1,14 +1,12 @@
 package at.spengergasse.weiss.services.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String home() {
         return "home return";
     }
