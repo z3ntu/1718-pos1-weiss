71aee6ef36f14693a62647ea82f2308449e4e19b
Luca Weiss <luca@z3ntu.xyz>
2018-02-14 21:04:37 +0100

    Cleanup

--- src/main/java/at/spengergasse/weiss/temperature/TemperatureConfiguration.java
+++ src/main/java/at/spengergasse/weiss/temperature/TemperatureConfiguration.java
@@ -4,12 +4,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.logging.Logger;
-
 @Configuration
 @ComponentScan
 @EntityScan("at.spengergasse.weiss.temperature")
-//@EnableJpaRepositories("at.spengergasse.weiss.temperature")
 public class TemperatureConfiguration {
-    protected Logger logger = Logger.getLogger(TemperatureConfiguration.class.getName());
 }
