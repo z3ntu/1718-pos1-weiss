a1b2c57ca3f9068f3fbfcc8d5d4cb3a9780b8598
Luca Weiss <luca@z3ntu.xyz>
2018-02-27 13:52:44 +0100

    Enable Hystrix dashboard
    Works by visiting http://localhost:1111, entering
    http://localhost:3333/hystrix.stream and making a request on the
    webserver

--- src/main/java/at/spengergasse/weiss/services/registration/RegistrationServer.java
+++ src/main/java/at/spengergasse/weiss/services/registration/RegistrationServer.java
@@ -4,10 +4,12 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableAdminServer
+@EnableHystrixDashboard
 public class RegistrationServer {
     public static void main(String[] args) {
         // Tell server to look for registration-server.properties or registration-server.yml
